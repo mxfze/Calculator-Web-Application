39605afd9460a742d03b72842984bbacdc48b5d8
Mofe <148377456+mxfze@users.noreply.github.com>
2024-04-16 17:09:31 +0100

    Allowed keyboard presses only for numbers and symbols

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function calculate(){
         display.value="Invalid";
     }
 }
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode !== 43 && charCode !== 45 && charCode !== 42 && charCode !== 47 && charCode !== 46) {
+      return false;
+    }
+    return true;
+  }
